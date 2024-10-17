90f7ab309ce47dd38bfe310a7f9c3a47e080441d
amGroot-dot <amgroot155@gmail.com>
2024-10-17 22:27:28 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -16,29 +16,52 @@ ZOHO.CREATOR.init()
         console.log(error);
       }
 
+      
     }
+
+    const myFunction = async (url) => {
+      config = {
+        action: "open",
+        url: "https://creatorapp.zoho.in/app_zubcon/zubcon-backup-j25/#Form:" + url,
+        window: "same"
+      }
+
+      await ZOHO.CREATOR.UTIL.navigateParentURL(config);
 
 
 // Append Item list in the UI
-    const appendItems = (all_items) => {
-      const list = document.querySelector(".list");
-      let tag = "";
-      let CreateNew = "<h6>Create New</h6>";
-      let viewUpdate = "<h6> View | Update</h6>";
-      for (let i = 0; i < all_items.length; i++) {
-        if (all_items[i].Type_field === "Create New") {
-          
-          CreateNew += `<a href="https://creatorapp.zoho.in/app_zubcon/zubcon-backup-j25/#Form:${all_items[i].Link_Name}" class="item text-dark" target='_top'>${all_items[i].Name}</a>`;
-        } else if (all_items[i].Type_field === "View | Update") {
-          viewUpdate += `<a href="https://creatorapp.zoho.in/app_zubcon/zubcon-backup-j25/#Report:${all_items[i].Link_Name}" class="item text-dark" target='_top'>${all_items[i].Name}</a>`;
-        }
-      }
-      list.innerHTML = CreateNew + viewUpdate;
+const appendItems = (all_items) => {
+  const list = document.querySelector(".list");
+  list.innerHTML = ""; // Clear existing items
+
+  // Create separate containers for each category
+  const createNewContainer = document.createElement('div');
+  const viewUpdateContainer = document.createElement('div');
+
+  // Add headers for each section
+  createNewContainer.innerHTML = "<h6>Create New</h6>";
+  viewUpdateContainer.innerHTML = "<h6>View | Update</h6>";
+
+  // Iterate over all items
+  for (let i = 0; i < all_items.length; i++) {
+    const divWrapper = document.createElement('div'); // Create a div wrapper for each button
+    divWrapper.classList.add('button-wrapper'); // Add a class to the wrapper
+
+    const button = document.createElement('button');
+    button.textContent = all_items[i].Name;
+    button.addEventListener('click', () => myFunction(all_items[i].Link_Name));
+    button.classList.add('custom-button'); // Add a custom button class for styling
+
+    divWrapper.appendChild(button); // Append button to div wrapper
+
+    // Append buttons to the appropriate section based on Type_field
+    if (all_items[i].Type_field === "Create New") {
+      createNewContainer.appendChild(divWrapper);
+    } else if (all_items[i].Type_field === "View | Update") {
+      viewUpdateContainer.appendChild(divWrapper);
     }
-    document.addEventListener("DOMContentLoaded", async () => {
-      const nameArr = await getRecords();
-      appendItems(nameArr);
-    })
+  }
+
 
 
     // Input Actions
